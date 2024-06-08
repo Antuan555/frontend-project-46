d393d154e727028597d117bf5c6e2b794dd48c88
Antuan555 <Antuan55678900@gmail.com>
2024-06-08 10:35:40 +0300

    Step 3

--- gendiff.js
+++ gendiff.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
-import { program } from 'commander'
+import { program } from 'commander';
+import path from 'path';
+import fs from 'fs';
+
+const action = (args) => args.map((e) => {
+  const pathFile = path.resolve(process.cwd(), e);
+  const data = fs.readFileSync(pathFile);
+  console.log(JSON.parse(data));
+  return JSON.parse(data);
+});
+
 program
-	.version('0.0.1')
-	.arguments('<filepath1> <filepath1>')
-	.description('Compares two configuration files and shows a difference.')
-	.helpOption('-h, --help', 'output usage information')
-	.option('-f, --format [type]',  'output format')
-	.parse(process.argv);
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
+
+const { args } = program;
+action(args);
